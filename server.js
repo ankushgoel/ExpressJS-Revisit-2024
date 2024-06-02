6f5427ac988a9c3d2497a22f3eb9095ae62f5a22
Ankush Goel <goelankush1312@gmail.com>
2024-06-03 01:18:02 +0530

    Added express static server and made api routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const PORT = 8080;
 const app = express();
 
+// Manual routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
     // res.send('Hello World!');
@@ -13,6 +14,34 @@ app.get('/about', (req, res) => {
     // res.send('About!');
 })
 
+// static server for autoloading public files with extension
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// For using express as a backend
+const posts = [
+    { id: 1, title: "Post 1"},
+    {id: 2, title: "Post 2"},
+    {id: 3, title: "Post 3"}
+]
+
+app.get('/api/posts', (req, res) => {
+    const limit = parseInt(req.query.limit);
+    if(!isNaN(limit) && limit > 0) {
+        res.json(posts.slice(0, limit))
+    } else 
+        res.json(posts);
+})
+
+app.get('/api/posts/:id', (req, res) => {
+    const postId = parseInt(req.params.id);
+    const post = posts.filter((post) => post.id === postId)
+    // console.log(post); // filter - [], find - undefined
+    if(!post || post.length < 1) {
+        res.status(404).json({message: `Post could not be found!`})
+    } else 
+        res.json(post);
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT: ${PORT}`);
