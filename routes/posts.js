27d046f148b24a6f54b0ae6a723bb58132c4efed
Ankush Goel <goelankush1312@gmail.com>
2024-06-03 21:52:01 +0530

    CRUD operations and logger middleware for single route

--- routes/posts.js
+++ routes/posts.js
@@ -2,13 +2,19 @@ import express from 'express';
 const router = express.Router();
 
 // For using express as a backend
-const posts = [
+let posts = [
     { id: 1, title: "Post 1"},
     {id: 2, title: "Post 2"},
     {id: 3, title: "Post 3"}
 ]
 
-router.get('/', (req, res) => {
+// Logger Middleware
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+}
+
+router.get('/', logger, (req, res) => {
     const limit = parseInt(req.query.limit);
     if(!isNaN(limit) && limit > 0) {
         res.json(posts.slice(0, limit))
@@ -24,6 +30,43 @@ router.get('/:id', (req, res) => {
         res.status(404).json({message: `Post could not be found!`})
     } else 
         res.json(post);
+})
+
+// Create new post
+router.post('/', (req, res) => {
+    // console.log(req.body);
+    if(req.body?.title) {
+        const newPost = {
+            id: posts.length+1,
+            title: req.body.title
+        }
+        posts.push(newPost)
+        res.status(201).json(newPost);
+    } else {
+        res.status(400).json({message: `Appropriate data not received for post request!`});
+    }  
+})
+
+// Create new post
+router.put('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+    if(!post) {
+        return res.status(400).json({message:`Post not found`});
+    }
+    post.title = req.body.title
+    res.status(200).json(post);
+})
+
+// Create new post
+router.delete('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+    if(!post) {
+        return res.status(400).json({message:`Post not found`});
+    }
+    posts = posts.filter((post) => post.id !== id)
+    res.status(200).json(posts);
 })
 
 // module.exports = router;
--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const app = express();
 const __filename = url.fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
+// Body Parser Middleware
+app.use(express.json());
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: false}));
+
 // Manual routes
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'))
