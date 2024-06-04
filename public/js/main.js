d33826075ff67c77a847a2e7015e200a86d01ce2
Ankush Goel <goelankush1312@gmail.com>
2024-06-04 20:44:00 +0530

    Add new post from homepage(frontend)

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,9 @@
 const btn = document.querySelector('#get-posts-btn')
 const output = document.querySelector('#output')
+const form = document.querySelector('#add-post-form')
 
-console.log(btn);
+btn.addEventListener('click', showPosts);
+form.addEventListener('submit', addPost);
 
 async function showPosts() {
     try {
@@ -24,4 +26,27 @@ async function showPosts() {
     
 }
 
-btn.addEventListener('click', showPosts);
+// Add new post
+async function addPost(e) {
+    e.preventDefault();
+    const formData = new FormData(this);
+    const title = formData.get('title');
+    try {
+        const res = await fetch('http://localhost:8080/api/posts', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({title})
+        })
+        console.log(res);
+        if(!res.ok) {
+            throw new Error('Failed to add post!');
+        }
+        // const newPost = await res.json();
+        // const postEle = document.createElement('div');
+        // postEle.textContent = newPost.title;
+        //     output.appendChild(postEle);
+        showPosts();
+    } catch (error) {
+        console.log('error while adding post!');
+    }
+}
